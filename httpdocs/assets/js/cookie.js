28fff0cec21a7da1bd7638b9850819dfefd3004d
thunfisch987 <thunfisch987@gmail.com>
2021-03-01 14:01:01 +0100

    fixed my cookies

--- httpdocs/assets/js/cookie.js
+++ httpdocs/assets/js/cookie.js
@@ -1,15 +1,12 @@
 function setcookie() {
-  document.cookie = "hideCookieNotice=1";
-  document.cookie = "path=/";
-  document.cookie = "expires=Fri, 31 Dec 9999 23:59:59 GMT"
-  document.cookie = "moto-moto likes you=''"
+  document.cookie = "hideCookieNotice=1; SameSite=Lax; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
   jQuery('#cookieNotice').slideUp();
 }
 
 if(document.cookie.indexOf('hideCookieNotice=1') != -1){
-    jQuery('#cookieNotice').hide();
+  jQuery('#cookieNotice').hide();
 }
 else{
-    jQuery('#cookieNotice').prependTo('body');
-    jQuery('#cookieNoticeCloser').show();
+  jQuery('#cookieNotice').prependTo('body');
+  jQuery('#cookieNoticeCloser').show();
 }
