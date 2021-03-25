9de7cec32597d93899229b81c61e2423eeb47a75
thunfisch987 <thunfisch987@gmail.com>
2021-03-25 21:34:15 +0100

    r4e (razer4ever) website stuff

---
+++ r4e/assets/js/sticky.js
@@ -0,0 +1,22 @@
+// get the sticky element
+/*const stickyElm = document.getElementById('navigationbar')
+const magnet = document.getElementById('magnet')
+
+const observer = new IntersectionObserver(
+  ([e]) => e.target.classList.toggle('isSticky', e.intersectionRatio < 1),
+  {threshold: [1]};
+  magnet.toggle("isSticky", stickyElm.intersectionRatio < 1), {treshold: [1]};
+);
+
+observer.observe(stickyElm)*/
+
+var observer = new IntersectionObserver(function(entries) {
+	// no intersection with screen
+	if(entries[0].intersectionRatio === 0)
+		document.querySelector("#magnet").classList.add("isSticky");
+	// fully intersects with screen
+	else if(entries[0].intersectionRatio === 1)
+		document.querySelector("#magnet").classList.remove("isSticky");
+}, { threshold: [0,1] });
+
+observer.observe(document.querySelector("#navtop"));
