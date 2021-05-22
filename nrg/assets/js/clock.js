a9b810e3bac870f4b01bc9ee59279468b3c9f3ae
thunfisch987 <thunfisch987@gmail.com>
2021-05-22 14:24:07 +0200

    added nrg/oldnrg

---
+++ nrg/assets/js/clock.js
@@ -0,0 +1,12 @@
+function showTime() {
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+
+    var time = h + ":" + m + ":" + s;
+    document.getElementById("clock").innerText = time;
+    document.getElementById("clock").textContent = time;
+
+    setTimeout(showTime, 1000);
+};
