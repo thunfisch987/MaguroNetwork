5d187c296885d9317b69fc3dfb81277e276b92d7
thunfisch987 <thunfisch987@gmail.com>
2021-07-30 01:17:03 +0200

    i hate js/jquery whateve

--- oldweeb/randommemes.js
+++ oldweeb/randommemes.js
@@ -1,10 +1,23 @@
-var allImages = new Array();
-$.ajax({url: "memes/", success: function (data) {
-    console.log({ data });
-		$(data).find("td > a").each(function () {
+// var allImages = new Array();
+// $.ajax({url: "memes/", success: function (data) {
+//     console.log({ data });
+// 		$(data).find("td > a").each(function () {
 				
-                var images = $(this).attr("href");
-                $('<p></p>').html(images)
-			});
-	},
-});
+//                 var images = $(this).attr("href");
+//                 $('<p></p>').html(images)
+// 			});
+// 	},
+// });
+
+$.ajax({
+    url: "memes/",
+    success: function(data){
+       $(data).find("a:contains(.jpg)").each(function(){
+          // will loop through 
+          var images = $(this).attr("href");
+  
+          $('<p></p>').html(images).appendTo('a div of your choice')
+  
+       });
+    }
+  });
