2ab07665a0394d7208be6aedeabbb110bd319be1
thunfisch987 <thunfisch987@gmail.com>
2021-07-30 00:19:35 +0200

    yeet

--- weeb/randommeme.js
+++ weeb/randommeme.js
@@ -1,22 +1,24 @@
-window.onload = choosePic;
+// window.onload = choosePic;
 
-var myPix = new Array("images/lion.jpg","images/tiger.jpg","images/bear.jpg");
+// var myPix = new Array("images/lion.jpg","images/tiger.jpg","images/bear.jpg");
 
-function choosePic() {
-     var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementById("myPicture").src = myPix[randomNum];
+// function choosePic() {
+//      var randomNum = Math.floor(Math.random() * myPix.length);
+//      document.getElementById("myPicture").src = myPix[randomNum];
 
-var path = 'memes/',
-imgs = ['carrot.gif','neil_1.jpg','steak.jpg'],
-i = Math.floor(Math.random()*imgs.length);
- $('.el').append("<img src='"+path+imgs[i]+"'>").hide().fadeIn(2000);
+// var path = 'memes/',
+// imgs = ['carrot.gif','neil_1.jpg','steak.jpg'],
+// i = Math.floor(Math.random()*imgs.length);
+//  $('.el').append("<img src='"+path+imgs[i]+"'>").hide().fadeIn(2000);
 
 $.ajax({
-    url: "memes/",
-    success: function(data){
-        $(data).find("td > a").each(function(){
-            // will loop through 
-            alert("Found a file: " + $(this).attr("href"));
-        });
-    }
+	url: "memes/",
+	success: function (data) {
+		$(data)
+			.find("td > a")
+			.each(function () {
+				// will loop through
+				alert("Found a file: " + $(this).attr("href"));
+			});
+	},
 });
