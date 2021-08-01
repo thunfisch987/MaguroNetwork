64e593647ae68ac500ab3c3e3060e977109b9101
thunfisch987 <thunfisch987@gmail.com>
2021-08-01 03:00:49 +0200

    hmpf 2

--- oldweeb/upload.php
+++ oldweeb/upload.php
@@ -17,10 +17,10 @@ $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 // }
 
 // Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
+// if (file_exists($target_file)) {
+//     echo "Sorry, file already exists.";
+//     $uploadOk = 0;
+// }
 
 // // Check file size
 // if ($_FILES["fileToUpload"]["size"] > 500000) {
