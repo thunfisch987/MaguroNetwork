70c815c97cdaa62a736e0364d1447d866ff18ecc
thunfisch987 <thunfisch987@gmail.com>
2021-08-26 20:03:51 +0200

    pwa v1.1

--- rndmemes/assets/js/main.js
+++ rndmemes/assets/js/main.js
@@ -3,6 +3,6 @@ window.onload = () => {
 	"use strict";
 
 	if ("serviceWorker" in navigator) {
-		navigator.serviceWorker.register("./sw.js");
+		navigator.serviceWorker.register("./assets/js/sw.js");
 	}
 };
