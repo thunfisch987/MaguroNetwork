47a281681e28eafc82081229345db27d972556d1
Oliver Sader <thunfisch987@gmail.com>
2021-10-25 14:05:58 +0200

    adjusting for german

--- nrg/assets/js/greeting.js
+++ nrg/assets/js/greeting.js
@@ -1,17 +1,17 @@
-window.onload = ( function() {
-  changegreeting();
+window.onload = (function () {
+	changegreeting();
 })();
 
 function changegreeting() {
-  var now = new Date();
-  var hour = now.getHours();
-  if(hour >= 5 && hour <= 12) {
-    document.getElementById('greeting').textContent = "Good Morning!"
-  } else if (hour >= 12 && hour <= 17) {
-    document.getElementById('greeting').textContent = "Good Afternoon!"
-  } else if (hour >= 17 && hour <= 22) {
-    document.getElementById('greeting').textContent = "Good Evening!"
-  } else if ((hour >= 0 && hour <= 5) || hour == 23) {
-    document.getElementById('greeting').textContent = "Good Night!"
-  }
+	var now = new Date();
+	var hour = now.getHours();
+	if (hour >= 5 && hour <= 12) {
+		document.getElementById("greeting").textContent = "Guten Morgen!";
+	} else if (hour >= 12 && hour <= 17) {
+		document.getElementById("greeting").textContent = "Guten Tag!";
+	} else if (hour >= 17 && hour <= 22) {
+		document.getElementById("greeting").textContent = "Guten Abend!";
+	} else if ((hour >= 0 && hour <= 5) || hour == 23) {
+		document.getElementById("greeting").textContent = "Gute Nacht!";
+	}
 }
