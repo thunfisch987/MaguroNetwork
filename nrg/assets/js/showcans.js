64f6f1a9afc4982021a90ff75cffa46df3385e3a
Oliver Sader <thunfisch987@gmail.com>
2021-10-31 01:02:49 +0200

    now using one showcans for all brands (using jquery)

--- nrg/assets/js/showcans.js
+++ nrg/assets/js/showcans.js
@@ -1,45 +1,38 @@
 /* jshint esversion: 11*/
+// @ts-check
+
 $("button").on("click", function (e) {
 	e.stopPropagation();
-	var div = $(`#${$(this).data("toggletype")}`);
-	var alldivs = $(".nrgs").not(div).hide();
-	alldivs.not(div).hide();
+	const div = $(`#${$(this).data("toggletype")}`);
+	$(".nrgs").not(div).hide();
+	const elems = div.find("img");
 	if (div.is(":hidden")) {
-		div.show(0, function () {
-			fadethemIN($(this).find("img").hide());
+		div.show(0, () => {
+			elems.hide();
+			elems.each(function (index) {
+				$(this)
+					.delay(400 * index)
+					.show("fade");
+			});
 		});
 	} else if (div.is(":visible")) {
-		fadingOUT(div.find("img"), div);
-		// $(div).hide(0);
+		let count = elems.length;
+		elems.each(function (index) {
+			$(this)
+				.delay(400 * index)
+				.hide("fade", () => {
+					if (index === count - 1) {
+						div.hide();
+					}
+				});
+		});
 	}
 });
 
 function fadethemIN(elems) {
-	// elems.eq(0).fadeIn(300, function () {
-	// 	fadethemIN(elems.slice(1));
-	// });
 	elems.each(function (index) {
 		$(this)
 			.delay(400 * index)
-			.fadeIn(300);
+			.show("fade");
 	});
-}
-
-function fadingOUT(elems, div) {
-	fadethemOUT(elems, function () {
-		div.hide();
-	});
-}
-
-function fadethemOUT(elems, callback) {
-	// elems.eq(0).fadeOut(300, function () {
-	// 	console.log(elems);
-	// 	fadethemOUT(elems.slice(1));
-	// });
-	elems.each(function (index) {
-		$(this)
-			.delay(400 * index)
-			.fadeOut(300);
-	});
-	callback();
 }
