874bb052a0663982bd0c3e0fc292f4dc9ae8879a
Oliver Sader <thunfisch987@gmail.com>
2021-10-31 16:34:37 +0100

    removing more junk (see last commit)

--- nrg/assets/js/tooltiplistener.js
+++
@@ -1,9 +0,0 @@
-/*jshint esversion: 6 */
-function tooltiplistener() {
-	const tooltipTriggerList = [].slice.call(
-		document.querySelectorAll('[data-bs-toggle="tooltip"]')
-	);
-	const tooltipList = tooltipTriggerList.map(
-		(tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
-	);
-}
