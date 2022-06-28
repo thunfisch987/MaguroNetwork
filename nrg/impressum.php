41c4e7c79d933886bc88f7a9cfb5df5a6283d226
thunfisch987 <43795814+thunfisch987@users.noreply.github.com>
2022-06-28 15:12:36 +0200

    oops, wrong var name

--- nrg/impressum.php
+++ nrg/impressum.php
@@ -6,7 +6,7 @@
 	$doc->loadHTML($theHtmlToParse);
 	$xpath = new DOMXpath($doc);
 	$elements = $xpath->query("//event-calender");
-	if($result->length > 0) {
+	if($lements->length > 0) {
 		$node = $result->item(0);
 		echo "{$node->nodeName} - {$node->nodeValue}";
 	}
