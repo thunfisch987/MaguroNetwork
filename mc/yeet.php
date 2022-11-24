78efe2ca0559740288ed05c5415d6d740fbc13a0
Oliver Sader <thunfisch987@gmail.com>
2022-11-24 17:39:59 +0100

    formatting

--- mc/yeet.php
+++ mc/yeet.php
@@ -9,23 +9,25 @@
 </head>
 
 <body>
-<?php
+    <?php
 
-$url = "http://mc.phnx.xyz:30814/networkElement/integrateddynamics/part/4";
+    $url = "http://mc.phnx.xyz:30814/networkElement/integrateddynamics/part/4";
 
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-//for debug only!
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    //for debug only!
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-$resp = curl_exec($curl);
-curl_close($curl);
-$resp2 = json_decode($resp);
-var_dump($resp2->value);
-?>
+    $resp = curl_exec($curl);
+    curl_close($curl);
+    $resp2 = json_decode($resp);
+    foreach ($resp2 as $value) {
+        echo explode(":",$value->resourceLocation)[1] . ": " . $value->count;
+    };
+    ?>
 </body>
 
 </html>
