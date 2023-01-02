a0ec7338438bbe18272f48c6a59515a5c06c3c59
Oliver Sader <thunfisch987@gmail.com>
2023-01-02 20:11:57 +0100

    fix clock

--- nrg/assets/js/clock.js
+++ nrg/assets/js/clock.js
@@ -3,9 +3,13 @@
 
 function showTime() {
 	const date = new Date();
-	const h = date.getHours(); // 0 - 23
-	const m = date.getMinutes(); // 0 - 59
-	const s = date.getSeconds(); // 0 - 59
+	let h = date.getHours(); // 0 - 23
+	let m = date.getMinutes(); // 0 - 59
+	let s = date.getSeconds(); // 0 - 59
+
+	h = (h < 10) ? `0${h}` : h;
+    m = (m < 10) ? `0${m}` : m;
+    s = (s < 10) ? `0${s}` : s;
 
 	const time = `${h}:${m}:${s}`;
 	document.getElementById("clock").innerText = time;
