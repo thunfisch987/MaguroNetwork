e437d36ad140a5101266075433d4364ef4e2d6e5
thunfisch987 <43795814+thunfisch987@users.noreply.github.com>
2023-04-22 02:19:06 +0200

    create twitch api access thingie

---
+++ httpdocs/api/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$username = "littlebitgayTV"
+$url = "https://api.twitch.tv/helix/streams?user_login=" . $username;
+$authtoken = "Authorization: " . file_get_contents("./twitchtoken")
+
+//init curl
+$curl = curl_init($url);
+//url setzen
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "Client-Id: wy7kx5tiaxglutzj1uucapo0vne0rm",
+   $authtoken
+);
+
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+$respjson = json_decode($resp)
+var_dump($respjson)
+
+?>
